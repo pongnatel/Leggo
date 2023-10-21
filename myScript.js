c4913f905c7076a1eebbf193648e7c5163ccab3e
pongnatel <s3877819@rmit.edu.vn>
2023-10-21 17:29:09 +0700

    fix number

--- myScript.js
+++ myScript.js
@@ -15,14 +15,15 @@ function updateResult() {
         totalCo2Emission += value * avg_co2_factor;
       }
     });
-  
+    
+    totalKm = totalKm.toFixed(2);
     totalCo2Emission = totalCo2Emission.toFixed(2); // Round the total emissions to 2 decimal places
   
     if (totalCo2Emission > 0) {
       totalDiv.innerHTML = `Total: <span class="green-text">${totalKm} km</span>`;
       resultDiv.innerHTML = `<span class="green-text">${totalCo2Emission} kg</span> of CO2`;
     } else {
-      totalDiv.innerHTML = 'Total: 0 km';
+      totalDiv.innerHTML = 'Total: 0  km';
       resultDiv.innerHTML = `0 kg of CO2`;
     }
   }
