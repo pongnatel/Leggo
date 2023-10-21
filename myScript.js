575c3d3cb48abaeb62cfb41a79039dd45017f99d
pongnatel <s3877819@rmit.edu.vn>
2023-10-21 17:23:27 +0700

    add total km

--- myScript.js
+++ myScript.js
@@ -1,44 +1,28 @@
 const avg_co2_factor = 0.11337;
 
-// Function to update the result when the input changes
-// function updateResult() {
-//   const inputElements = document.querySelectorAll("input"); // Get all input elements
-//   const resultDiv = document.getElementById("result");
-//   let co2Emission = 0;
-
-//   inputElements.forEach(function (inputElement) {
-//     var value = parseFloat(inputElement.value); // Parse the input value as a float
-
-//     if (!isNaN(value)) {
-//       co2Emission += (value * 0.113).toFixed(2); // Calculate CO2 emissions for each day
-//     }
-//   });
-
-//   if (co2Emission > 0) {
-//     resultDiv.innerHTML = `<span class="green-text">${co2Emission} kg</span> of CO2`;
-//   } else {
-//     resultDiv.innerHTML = `0 kg of CO2`;
-//   }
-// }
-
 function updateResult() {
     const inputElements = document.querySelectorAll("input");
+    const totalDiv = document.getElementById("total");
     const resultDiv = document.getElementById("result");
+    let totalKm = 0;
     let totalCo2Emission = 0;
   
     inputElements.forEach(function (inputElement) {
       const value = parseFloat(inputElement.value);
       
       if (!isNaN(value)) {
-        totalCo2Emission += value * 0.113;
+        totalKm += value;
+        totalCo2Emission += value * avg_co2_factor;
       }
     });
   
     totalCo2Emission = totalCo2Emission.toFixed(2); // Round the total emissions to 2 decimal places
   
     if (totalCo2Emission > 0) {
+      totalDiv.innerHTML = `Total: <span class="green-text">${totalKm} km</span>`;
       resultDiv.innerHTML = `<span class="green-text">${totalCo2Emission} kg</span> of CO2`;
     } else {
+      totalDiv.innerHTML = 'Total: 0 km';
       resultDiv.innerHTML = `0 kg of CO2`;
     }
   }
